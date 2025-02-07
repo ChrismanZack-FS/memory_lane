d461155127aa9e3fa867df21f1b2ec35ca865a6e
Zack Chrisman <zjchrisman@student.fullsail.edu>
2025-02-07 17:02:53 -0600

    working on getting images to work

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
 	output: "export",
 	distDir: "dist",
 	images: { unoptimized: true },
+	basePath: isProd ? "/memory_lane" : "",
 };
 
 module.exports = nextConfig;
