6f149704590f627128571fc375e5928b8bacc535
Zack Chrisman <zjchrisman@student.fullsail.edu>
2025-02-07 19:30:23 -0600

    I cant get the images to load

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
 	distDir: "dist",
 	images: { unoptimized: true },
 	basePath: isProd ? "/memory_lane" : "",
+	env: {
+		NEXT_PUBLIC_BASE_PATH: isProd ? "/memory_lane" : "",
+	},
 };
 
 module.exports = nextConfig;
